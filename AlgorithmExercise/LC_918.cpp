f7ff30c55f637d75f5f67124f4bdae5026222ee5
Leeeeein <704890093@qq.com>
2022-04-05 20:27:35 +0800

    LC_918

---
+++ AlgorithmExercise/LC_918.cpp
@@ -0,0 +1,34 @@
+#include "SolutionBaseClass.cpp"
+
+class Solution : public Solution
+{
+public:
+	int maxSubarraySumCircular(vector<int>& nums) {
+		int maxV = INT_MIN, minV = INT_MAX, cnt = 0;
+		int m = INT_MIN;
+		vector<int> dp_max(nums.size(), 0);
+		vector<int> dp_min(nums.size(), 0);
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (nums[i] > m)
+				m = nums[i];
+			cnt += nums[i];
+			if (i == 0)
+			{
+				dp_max[i] = nums[i];
+				dp_min[i] = nums[i];
+			}
+			else
+			{
+				dp_max[i] = max(dp_max[i - 1] + nums[i], nums[i]);
+				dp_min[i] = min(dp_min[i - 1] + nums[i], nums[i]);
+			}
+			maxV = max(maxV, dp_max[i]);
+			minV = min(minV, dp_min[i]);
+		}
+		if (m <= 0)
+			return m;
+		return max(maxV, cnt - minV);
+	}
+};
